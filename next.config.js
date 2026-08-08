b8b6ed94f771e0ac612af09f7b93b31015bcd351
HengChanVireakYuth <chanvireakyuthheng@gmail.com.com>
2026-08-08 10:13:02 +0700

    fix mongodb dns connection

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
